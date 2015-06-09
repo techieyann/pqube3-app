ebca5d1948cd80cafbbab04d511718c5907d3c2b
Ian McEachern <techieyann@gmail.com>
2015-06-08 20:07:33 -0700

    needle start at bottom of gauge, 7seg size fix

--- pqube3.js
+++ pqube3.js
@@ -11,19 +11,18 @@ if (Meteor.isClient) {
   });
 
 	Meteor.subscribe('freq');
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 	var freqGauge;
 	
 	Template.gauge.rendered = function () {
-		console.log('rendered');
 		var self = this;
 		self.gauge = new TunguskaGauge({
 			id: 'freq-gauge',
 			theme: 'basic',
 			range: {
 				min: 59.9,
+				lowStop: 59.98,
 				max: 60.1,
+				highStop: 60.11,
 				sweep: 240,
 				startAngle: -120
 			},
@@ -44,8 +43,8 @@ if (Meteor.isClient) {
 			},
 			tick: {
 				major: {
-					startAt: 1,
-					endAt: 1,
+					startAt: 0,
+					endAt: 0,
 					first: 59.9,
 					last: 60.1,
 					interval: .1,
@@ -60,12 +59,12 @@ if (Meteor.isClient) {
 		});
 		self.gauge.theme.pointer.dynamics = {
 			easing: 'easeOutQuint',
-			duration: 250
+			duration: 500
 		};
-		self.gauge.set(60);
+		self.gauge.set(59.98);
 		freqGauge = self.gauge;
 	};
-  Template.hello.helpers({
+  Template.freq.helpers({
 		frequency: function () {
 			var data = Freq.findOne();
 			if (data) {
@@ -85,49 +84,48 @@ if (Meteor.isServer) {
 	var DecoderRing = Meteor.npmRequire('decoder-ring');
 	var decoderRing = new DecoderRing();
 	
-var RIR = bus.FUNCTION_CODES.READ_INPUT_REGISTERS;
+	var RIR = bus.FUNCTION_CODES.READ_INPUT_REGISTERS;
 
-// IP and port of the MODBUS slave, default port is 502
-var IP = process.env.pqubeIP;
-var client = Meteor.npmRequire('modbus-stack/client').createClient(502, IP);
-Meteor.setInterval(function () {
+	// IP and port of the MODBUS slave, default port is 502
+	var IP = process.env.pqubeIP;
+	var client = Meteor.npmRequire('modbus-stack/client').createClient(502, IP);
+	Meteor.setInterval(function () {
+		// 'req' is an instance of the low-level `ModbusRequestStack` class
+		var req = client.request(RIR, // Function Code: 4
+														 9020,    // Start at address 9020
+														 2);  // Read 2 contiguous registers from 9020;
+
+		var reqAsync = Meteor.wrapAsync(req.on, req);
+
+		reqAsync('response', function(registers) {
+			var freq = calcFreq(registers);
+			Freq.upsert({_id: 'frequency'}, {$set: freq});
+		});
+	}, 501);
 
 
-// 'req' is an instance of the low-level `ModbusRequestStack` class
-var req = client.request(RIR, // Function Code: 4
-                         9020,    // Start at address 9020
-                         2);  // Read 2 contiguous registers from 9020;
 
-var reqAsync = Meteor.wrapAsync(req.on, req);
-
-reqAsync('response', function(registers) {
-	var freq = calcFreq(registers);
-	Freq.upsert({_id: 'frequency'}, {$set: freq});
-
-});
-}, 501);
-	var spec = {
-		bigEndian: true,
-		fields: [
-			{name: 'freq', start:0, type:'float'}
-		]
+	var calcFreq = function(registers) {
+		var octets = registersToOctets(registers);
+		var buff = new Buffer(octets);
+		return decoderRing.decode(buff, 
+															{
+																bigEndian: true,
+																fields: [
+																	{name: 'freq', start:0, type:'float'}
+																]
+															});
 	};
 
-var calcFreq = function(registers) {
-	var octets = registersToOctets(registers);
-	var buff = new Buffer(octets);
-	return decoderRing.decode(buff, spec);
-};
-
-var registersToOctets = function (registers) {
-	registers.splice(2,2);
-	var octets = [];
-	for (var i=0;i<2;i++) {
-		octets.push(registers[i]>>8);
-		octets.push(registers[i]>>0 & 0xff);
-	}
-	return octets;
-}
+	var registersToOctets = function (registers) {
+		registers.splice(2,2);
+		var octets = [];
+		for (var i=0;i<2;i++) {
+			octets.push(registers[i]>>8);
+			octets.push(registers[i]>>0 & 0xff);
+		}
+		return octets;
+	};
 
 
   Meteor.startup(function () {
