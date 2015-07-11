42308d34934e5d32e88ff671e298ff4b4646c3ec
Ian McEachern <techieyann@gmail.com>
2015-07-11 09:58:44 -0700

    units translation init

--- client/templates/gauges/js/gauge.js
+++ client/templates/gauges/js/gauge.js
@@ -10,7 +10,7 @@ Template.gauge.onRendered(function () {
       $('#'+data.prefix+'-display').sevenSeg(data.sevenSegment);
       var tgOpts = data.tunguskaGauge;
       tgOpts.digital.callback =  function (pV) {
-	return data.tgDigitalUnits;
+	return TAPi18n.__(data.tgDigitalUnits);
       };
       tgOpts.tick.major.legend.callback = function (n) {
 	return  n.toFixed(data.tgMajorLegendSigFigs);
