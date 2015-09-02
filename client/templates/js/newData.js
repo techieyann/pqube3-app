e98b126aa822c72af880428472d16519fa7b3c70
Ian McEachern <techieyann@gmail.com>
2015-09-01 21:13:51 -0700

    place status lights in DOM, alternatively hide

--- client/templates/js/newData.js
+++ client/templates/js/newData.js
@@ -1,4 +1,9 @@
 Template.newData.helpers({
+  hidden: function(light) {
+    if (light == 'on' && Session.get('light_on')) return 'hidden';
+    if (light == 'off' && !Session.get('light_on')) return 'hidden';
+    return '';
+  },
   lightStatus: function () {
     return (Session.equals('light_on', true) ? 'on':'off');
   }
