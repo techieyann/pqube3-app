8e8fcd631fcd3084e554d52cb1f6993a95242879
Ian McEachern <techieyann@gmail.com>
2015-09-29 11:53:38 -0700

    generalized scaling to accept all numbers

--- client/js/125scaling.js
+++ client/js/125scaling.js
@@ -2,14 +2,16 @@ down125 = function (num) {
   var sciNot = num.toExponential().split('e');
   var coefficient = parseInt(sciNot[0]);
   var exponent = parseInt(sciNot[1]);
-  if (coefficient == 1) {
+  if (coefficient <= 1) {
     exponent--;
     coefficient = 5;
   }
-  else if (coefficient == 2) 
+  else if (coefficient <= 2) 
     coefficient = 1;
-  else if (coefficient == 5)
+  else if (coefficient <= 5)
     coefficient = 2;
+  else if (coefficient < 10)
+    coefficient = 5;
   else {
     console.log('125 scaling down error: '+num);
     return;
@@ -22,13 +24,13 @@ up125 = function (num) {
   var sciNot = num.toExponential().split('e');
   var coefficient = parseInt(sciNot[0]);
   var exponent = parseInt(sciNot[1]);
-  if (coefficient == 5) {
+  if (coefficient >= 5) {
     coefficient = 1;
     exponent++;
   }
-  else if (coefficient == 2) 
+  else if (coefficient >= 2) 
     coefficient = 5;
-  else if (coefficient == 1)
+  else if (coefficient >= 1)
     coefficient = 2;
   else {
     console.log('125 scaling up error: '+num);
