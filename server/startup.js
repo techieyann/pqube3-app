b67bf77384ba7892624681977ba3597d0c440262
Ian McEachern <techieyann@gmail.com>
2016-01-20 11:24:06 -0800

    remove visiblity fix

--- server/startup.js
+++ server/startup.js
@@ -1,5 +1,4 @@
 Meteor.startup(function () {
-  PQubes.update({}, {$set: {visibility: 'public'}}, {multi:true});
   PQubes.update({status: {$nin: ['unverified','unknown']}}, {$set: {status: 'disconnected'}}, {multi: true});
   observePQubes();
   
